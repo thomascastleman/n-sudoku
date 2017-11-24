9193785f5b793996dea15f139eb4c9fd7436156e
Thomas Castleman <thomas7rex@gmail.com>
2017-11-24 18:38:45 -0500

    Implemented most of position class

--- src/sudoku/EmptyPosition.java
+++ src/sudoku/EmptyPosition.java
@@ -1,5 +1,7 @@
 package sudoku;
 
+import java.util.*;
+
 public class EmptyPosition {
 	
 	public int value;				// current numeric value held by this position
@@ -9,23 +11,66 @@ public class EmptyPosition {
 	public int numConflicts;		// current number of conflicts posed by this position's value
 	public int[] possibleValues;	// all possible values this position may hold, which do not violate the clue grid values
 	
+	public SudokuGrid parentGrid;	// reference to grid that contains this position
+	
 	public EmptyPosition() {
 		
 	}
 	
 	// calculate the number of conflicts given a value of this position
 	public int calcConflictWithValue(int value) {
-		return 0;
+		int conflict = 0;
+		
+		for (ArrayList<EmptyPosition> set : Arrays.asList(
+				parentGrid.tentativeRowVals.get(this.row), 
+				parentGrid.tentativeColVals.get(this.col), 
+				parentGrid.tentativeBlockVals.get(this.blockID))) {
+			
+			for (EmptyPosition p : set) {
+				if (p.value == value) {
+					conflict++;
+				}
+			}
+		}
+		
+		return conflict;
 	}
 	
 	// find another possible value of this empty position with less conflicts
-	public int getOtherValueOfLowestConflict() {
-		return 0;
+	public void updateToLowestConflictValue() {
+		for (int val : this.possibleValues) {
+			int conf = this.calcConflictWithValue(val);
+			if (conf < this.numConflicts) {
+				this.value = val;
+				this.numConflicts = conf;
+			}
+		}
+	}
+	
+	// update value and conflicts to random selection from possible
+	public void updateToRandomPossibleValue() {
+		this.value = this.possibleValues[(int) (Math.random() * this.possibleValues.length)];
+		this.numConflicts = this.calcConflictWithValue(this.value);
 	}
 	
 	// update the conflict values of all other EmptyPositions affected by this position's change in value
 	public void updateAffectedConflicts(int previousValue) {
+		// if value actually changed
+		if (previousValue != this.value) {
 		
+			for (ArrayList<EmptyPosition> set : Arrays.asList(
+					parentGrid.tentativeRowVals.get(this.row), 
+					parentGrid.tentativeColVals.get(this.col), 
+					parentGrid.tentativeBlockVals.get(this.blockID))) {
+				
+				for (EmptyPosition p : set) {
+					// if position had a conflict with previous value
+					if (p.value == previousValue) {
+						p.numConflicts--;		// remove that conflict
+					}
+				}
+			}
+		}
 	}
 
 }
